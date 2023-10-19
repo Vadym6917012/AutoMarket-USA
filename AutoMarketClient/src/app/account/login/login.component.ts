a6e74a14daa8a8385bc2fc20e397d67d4ce5af85
Vadym6917012 <vadimr69170@gmail.com>
2023-10-19 20:09:29 +0300

    ClientApp, user persistance in Angular

--- AutoMarketClient/src/app/account/login/login.component.ts
+++ AutoMarketClient/src/app/account/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AccountService } from '../account.service';
+import { take } from 'rxjs';
+import { User } from 'src/app/shared/models/user';
 
 @Component({
   selector: 'app-login',
@@ -15,7 +17,15 @@ export class LoginComponent implements OnInit {
 
   constructor(private accountService: AccountService, 
     private formBuilder: FormBuilder,
-    private router: Router) {}
+    private router: Router) {
+      this.accountService.user$.pipe(take(1)).subscribe({
+        next: (user: User | null ) => {
+          if (user) {
+            this.router.navigateByUrl('/');
+          }
+        }
+      });
+    }
     
   ngOnInit(): void {
     this.initializeForm();
@@ -35,8 +45,7 @@ export class LoginComponent implements OnInit {
     if (this.loginForm.valid){
       this.accountService.login(this.loginForm.value).subscribe({
         next: (response: any) => {
-          
-          console.log(response);
+          this.router.navigateByUrl('/');
         },
         error: error => {
           if (error.error.error){
--- AutoMarketClient/src/app/shared/components/modals/notification/notification.component.ts
+++ AutoMarketClient/src/app/shared/components/modals/notification/notification.component.ts
@@ -12,4 +12,8 @@ export class NotificationComponent {
   message: string = '';
 
   constructor(public bsModalRef: BsModalRef) {}
+
+  logout(){
+    
+  }
 }
